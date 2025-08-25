0763b14d7ae8b226263e04590fa536e4bfbe2bf7
Roman Yakovlev <yakovlev.roman@gmail.com>
2025-08-25 23:18:29 +0300

    minor changes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,6 +31,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define DS3231_I2C_ADDR 0x68
+
 #define NIXIE_REFRESH_MS 2
 /* USER CODE END PD */
 
@@ -55,8 +57,8 @@ static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
-void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c);
-void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c);
+RTC_TimeTypeDef ds3231_getTime(void);
+RTC_DateTypeDef ds3231_getDate(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -65,9 +67,9 @@ void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -109,13 +111,16 @@ int main(void)
   {
     currentMs = HAL_GetTick();
 
-    if ((currentMs - nixiePrevRefreshMs) >= NIXIE_REFRESH_MS)    {
+    if ((currentMs - nixiePrevRefreshMs) >= NIXIE_REFRESH_MS)
+    {
       nixiePrevRefreshMs = currentMs;
       nixie_refresh();
     }
 
-    if((currentMs - timePrevRefreshMs) >= 1000) { // Every second, change the digits
+    if ((currentMs - timePrevRefreshMs) >= 1000)
+    { // Every second, change the digits
       HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+      // ds3231_getTime(&sTime);
       nixie_setDigits((uint8_t[]){0, 0, sTime.Minutes / 10, sTime.Minutes % 10, sTime.Seconds / 10, sTime.Seconds % 10});
     }
 
@@ -127,9 +132,9 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -137,9 +142,9 @@ void SystemClock_Config(void)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.LSIState = RCC_LSI_ON;
@@ -152,9 +157,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -173,10 +177,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C1_Init(void)
 {
 
@@ -203,14 +207,13 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
-  * @brief RTC Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief RTC Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_RTC_Init(void)
 {
 
@@ -223,7 +226,7 @@ static void MX_RTC_Init(void)
   /* USER CODE END RTC_Init 1 */
 
   /** Initialize RTC Only
-  */
+   */
   hrtc.Instance = RTC;
   hrtc.Init.AsynchPrediv = RTC_AUTO_1_SECOND;
   hrtc.Init.OutPut = RTC_OUTPUTSOURCE_ALARM;
@@ -234,14 +237,13 @@ static void MX_RTC_Init(void)
   /* USER CODE BEGIN RTC_Init 2 */
 
   /* USER CODE END RTC_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -254,18 +256,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, D0_Pin|A4_Pin|D1_Pin|A5_Pin
-                          |D2_Pin|A6_Pin|LED_Pin|D5_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, D0_Pin | A4_Pin | D1_Pin | A5_Pin | D2_Pin | A6_Pin | LED_Pin | D5_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, D3_Pin|A2_Pin|D9_Pin|D8_Pin
-                          |D7_Pin|D6_Pin|D4_Pin|A1_Pin
-                          |A3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, D3_Pin | A2_Pin | D9_Pin | D8_Pin | D7_Pin | D6_Pin | D4_Pin | A1_Pin | A3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : D0_Pin A4_Pin D1_Pin A5_Pin
                            D2_Pin A6_Pin LED_Pin D5_Pin */
-  GPIO_InitStruct.Pin = D0_Pin|A4_Pin|D1_Pin|A5_Pin
-                          |D2_Pin|A6_Pin|LED_Pin|D5_Pin;
+  GPIO_InitStruct.Pin = D0_Pin | A4_Pin | D1_Pin | A5_Pin | D2_Pin | A6_Pin | LED_Pin | D5_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -274,9 +272,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : D3_Pin A2_Pin D9_Pin D8_Pin
                            D7_Pin D6_Pin D4_Pin A1_Pin
                            A3_Pin */
-  GPIO_InitStruct.Pin = D3_Pin|A2_Pin|D9_Pin|D8_Pin
-                          |D7_Pin|D6_Pin|D4_Pin|A1_Pin
-                          |A3_Pin;
+  GPIO_InitStruct.Pin = D3_Pin | A2_Pin | D9_Pin | D8_Pin | D7_Pin | D6_Pin | D4_Pin | A1_Pin | A3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -288,13 +284,42 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+RTC_TimeTypeDef ds3231_getTime(void)
+{
+  RTC_TimeTypeDef time;
 
+  uint8_t buffer[3];
+  HAL_I2C_Master_Transmit(&hi2c1, DS3231_I2C_ADDR << 1, 0, 1, HAL_MAX_DELAY);
+  HAL_I2C_Master_Receive(&hi2c1, DS3231_I2C_ADDR << 1, buffer, 3, HAL_MAX_DELAY);
+
+  time.Hours = ((buffer[2] >> 4) & 0x03) * 10 + (buffer[2] & 0x0F);
+  time.Minutes = ((buffer[1] >> 4) & 0x07) * 10 + (buffer[1] & 0x0F);
+  time.Seconds = ((buffer[0] >> 4) & 0x07) * 10 + (buffer[0] & 0x0F);
+
+  return time;
+}
+
+RTC_DateTypeDef ds3231_getDate(void)
+{
+  RTC_DateTypeDef date;
+
+  uint8_t buffer[4];
+  HAL_I2C_Master_Transmit(&hi2c1, DS3231_I2C_ADDR << 1, 3, 1, HAL_MAX_DELAY);
+  HAL_I2C_Master_Receive(&hi2c1, DS3231_I2C_ADDR << 1, buffer, 4, HAL_MAX_DELAY);
+
+  date.Year = ((buffer[3] >> 4) & 0x0F) * 10 + (buffer[3] & 0x0F);
+  date.Month = ((buffer[2] >> 4) & 0x01) * 10 + (buffer[2] & 0x0F);
+  date.Date = ((buffer[1] >> 4) & 0x03) * 10 + (buffer[1] & 0x0F);
+  date.WeekDay = buffer[0] & 0x07;
+
+  return date;
+}
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -307,12 +332,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
